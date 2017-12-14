dd662a29e52ee35bc2671081ef5c9960b766d5d4
PE\Luis Fabián <luisfbn@hotmail.com>
2017-12-14 17:00:25 -0600

    Styles

--- whatsweb_cs.js
+++ whatsweb_cs.js
@@ -6,7 +6,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 			document.getElementById("styleSFW").remove();
 		}
 		
-		
+		if( $('.m6ZEb').length ) {
+			$('.m6ZEb').remove();
+		}
 		
 		var css = document.createElement("style");
 		css.setAttribute("id", "styleSFW");
@@ -15,12 +17,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		css.innerHTML = "";
 		css.innerHTML += ".image-thumb {opacity: 0.05; transition: visibility 0s, opacity 0.5s linear;} ";
 		css.innerHTML += ".image-thumb:hover {opacity: 1;} ";
+		
+		//css.innerHTML += ".pane-body * {background-color: black!important; color: gray!important;} ";
+		
 		css.innerHTML += ".pane-body { background-image:url('http://listas.eleconomista.es/system/items/000/074/589/medium/E95.jpg?1464076950') !important; }";
 		
 		
-		css.innerHTML += ".chat-status { color: gray;} ";
-		css.innerHTML += ".chat-avatar { background-color: rgba(0, 0, 0, 0.7);} ";
-		css.innerHTML += ".chat-body { background-color: rgba(0, 0, 0, 0.7);} ";
+		//css.innerHTML += ".chat-status { color: gray;} ";
+		css.innerHTML += ".chat-avatar { background-color: rgba(0, 0, 0, 1);} ";
+		css.innerHTML += ".chat-body { background-color: rgba(0, 0, 0, 1);} ";
+		css.innerHTML += ".pane-chat-header { background-color: rgba(0, 0, 0, 1);} ";
+		
+		//experiment
+		css.innerHTML += ".ellipsify { color: white;} "; //.starred-title-main 
+		css.innerHTML += ".chat-status{ color: yellow;} "; // .chat-secondary, .header-secondary 
+		css.innerHTML += ".chat-body *{ color: gray!important;} ";
+		css.innerHTML += ".chatlist-panel-search *{ background-color: black!important; color: gray!important;} ";
+		
+		
+		css.innerHTML += ".pane-header.pane-list-header { background-color: rgba(0, 0, 0, 1);} ";
+		css.innerHTML += ".chatlist-panel-search { background-color: rgba(0, 0, 0, 1);} ";
+		
 		
 		css.innerHTML += ".app-wrapper { background-color: rgba(0, 0, 0, 0.7);} ";
 		css.innerHTML += ".bubble-image  { background-color: rgba(0, 0, 0, 0.7);} ";
@@ -28,7 +45,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		//css.innerHTML += ".two  {width: 10% !important;} "; //.k1feT
 		
 		document.body.appendChild(css);
-	
+		
     }
 
     if (request.action == "quitarEstilos") {
@@ -43,6 +60,24 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 chrome.runtime.sendMessage({ action: "show" });
+
+
+
+
+//formato tabla
+//var jq = document.createElement("script");
+//jq.setAttribute("id", "jqSFW");
+//jq.setAttribute("src", "http://www.jtable.org/Scripts/jtable/jquery.jtable.js");
+//document.body.appendChild(jq);
+
+//$('.pane-chat-msgs').dataTable({ "bPaginate": false });
+//$('.bubble').dataTable({ "bPaginate": false });
+//$('#main').dataTable({ "bPaginate": false });
+
+
+
+
+
 
 
 //https:code.jquery.com/jquery-1.10.2.js
